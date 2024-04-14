8cf016a1ed6c739e804592af1824548b4865a65c
Aapple21 <aapplegate12@icloud.com>
2024-04-14 13:45:02 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 //import Signup from "./Signup"
 // import Home from "./components/Home"
 import AfterPost from "./AfterPost"
+// import CalendarPage from "./Calendar"
 
 export default function App() {
   return (
     // <Home/>
     // <PostPage/>
     <AfterPost/>
+    // <CalendarPage/>
   )
 }
